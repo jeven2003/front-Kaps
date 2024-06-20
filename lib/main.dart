852200ac966bdfdf66576ed1b23443d077213757
jeven2003 <21-10161@g.batstate-u.edu.ph>
2024-06-20 12:10:30 +0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,68 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home:Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('Kaps App'),
-        ),
-        backgroundColor: Colors.brown,
-      ),
-      body: Center(
-        child: Image.network('https://vaya.in/recipes/wp-content/uploads/2018/05/Coffee.jpg'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.brown,
 
-        child: Icon (
-            Icons.add,
-            color: Colors.black,
-        ),
-
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    backgroundColor: Colors.grey,
+    appBar: AppBar(
+      title: Text ('User Profile'),
+      backgroundColor: Colors.brown[700],
+      centerTitle: true,
+    ),
+    body: Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'NAME',
+            style:TextStyle(
+              fontSize: 16.0,
+              letterSpacing: 1.0,
+            ) ,
+          ),
+          Text(
+            'John Vincent Eguia',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 25.0,
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          Text(
+            'YEAR',
+            style:TextStyle(
+              fontSize: 16.0,
+              letterSpacing: 1.0,
+            ) ,
+          ),
+          Text(
+            '3rd Year',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 25.0,
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          Text(
+            'EMAIL',
+            style:TextStyle(
+              fontSize: 16.0,
+              letterSpacing: 1.0,
+            ) ,
+          ),
+          Text(
+            'johneguia2003@gmail.com',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 25.0,
+            ),
+          ),
+        ],
       ),
     ),
-  ));
-}
-
-
-
+  ),
+));
