8ed5557d5c27acc2376c15a70dbd160cf7830988
jeven2003 <21-10161@g.batstate-u.edu.ph>
2024-06-26 11:45:20 +0800

    Add Dashboard and Menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:untitled2/home.dart';
+import 'package:untitled2/pages/dashboard.dart';
+import 'package:untitled2/pages/menu.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  routes: {
+    '/' : (context)=> Dashboard(),
+    '/menu': (context) => Menu(),
+    '/profile' : (context) => Home(),
+},
 ));
---
+++ lib/pages/dashboard.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text('Dashboard'),
+        backgroundColor: Colors.brown[700],
+        centerTitle: true,
+      ),
+      body: Container(
+        child: SafeArea(
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.pushNamed(context, '/menu');
+                },
+                child: Text('Go to Menu'),
+
+              ),
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.pushNamed(context, '/profile');
+                },
+                child: Text('Go to Profile'),
+
+              ),
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
