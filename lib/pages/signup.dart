7b558b7ca6a70a10456df05ec29d6fc42b792f52
jeven2003 <21-10161@gmail.com>
2024-07-09 12:03:07 +0800

    an account is already registered in this email

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:untitled2/Services/user.dart';
+import 'package:http/http.dart' as http;
+
 
 class signup extends StatefulWidget {
   const signup({super.key});
@@ -13,9 +18,25 @@ class _signupState extends State<signup> {
   String email = '';
   String password = '';
 
+
+  createAccount(User user)async{
+    final response = await http.post(
+      Uri.parse('http://10.0.2.2:8080/api/v1/Auth/register/user'),
+      headers : <String, String>{
+        'Content-Type' : 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'username' : user.username,
+        'email' : user.email,
+        'password' : user.password,
+      }),
+    );
+    print(response.body);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 0),
@@ -109,9 +130,12 @@ class _signupState extends State<signup> {
                         onPressed: (){
                           if(formKey.currentState!.validate()){
                             formKey.currentState!.save();
-                            print(name);
-                            print(email);
-                            print(password);
+                            User user = User(
+                              username: name,
+                              email: email,
+                              password: password,
+                            );
+                            createAccount(user);
                           }
                         },
                         child:  Text ('Signup'),
